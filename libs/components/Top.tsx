33b33264c7a4533596421b0af1029c9e31b4bc87
MakhmudSD <makhwork15@gmail.com>
2025-07-18 18:41:15 +0900

    fix: lan-box being modified

--- libs/components/Top.tsx
+++ libs/components/Top.tsx
@@ -18,177 +18,173 @@ import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined
 import { CaretDown } from 'phosphor-react';
 
 const Top = () => {
-  const device = useDeviceDetect();
-  const user = useReactiveVar(userVar);
-  const { t, i18n } = useTranslation('common');
-  const router = useRouter();
-  const [anchorEl2, setAnchorEl2] = useState<null | HTMLElement>(null);
-  const [lang, setLang] = useState<string | null>('en');
-  const drop = Boolean(anchorEl2);
-  const [colorChange, setColorChange] = useState(false);
-  const [bgColor, setBgColor] = useState<boolean>(false); // Still not directly used for a class change in the main div
-  const [logoutAnchor, setLogoutAnchor] = React.useState<null | HTMLElement>(null);
-  const logoutOpen = Boolean(logoutAnchor);
+	const device = useDeviceDetect();
+	const user = useReactiveVar(userVar);
+	const { t, i18n } = useTranslation('common');
+	const router = useRouter();
+	const [anchorEl2, setAnchorEl2] = useState<null | HTMLElement>(null);
+	const [lang, setLang] = useState<string | null>('en');
+	const drop = Boolean(anchorEl2);
+	const [colorChange, setColorChange] = useState(false);
+	const [bgColor, setBgColor] = useState<boolean>(false); // Still not directly used for a class change in the main div
+	const [logoutAnchor, setLogoutAnchor] = React.useState<null | HTMLElement>(null);
+	const logoutOpen = Boolean(logoutAnchor);
 
-  /** LIFECYCLES **/
-  useEffect(() => {
-    if (localStorage.getItem('locale') === null) {
-      localStorage.setItem('locale', 'en');
-      setLang('en');
-    } else {
-      setLang(localStorage.getItem('locale'));
-    }
-  }, [router]);
+	/** LIFECYCLES **/
+	useEffect(() => {
+		if (localStorage.getItem('locale') === null) {
+			localStorage.setItem('locale', 'en');
+			setLang('en');
+		} else {
+			setLang(localStorage.getItem('locale'));
+		}
+	}, [router]);
 
-  useEffect(() => {
-    switch (router.pathname) {
-      case '/product/detail':
-        setBgColor(true);
-        break;
-      default:
-        setBgColor(false);
-        break;
-    }
-  }, [router]);
+	useEffect(() => {
+		switch (router.pathname) {
+			case '/product/detail':
+				setBgColor(true);
+				break;
+			default:
+				setBgColor(false);
+				break;
+		}
+	}, [router]);
 
-  useEffect(() => {
-    const jwt = getJwtToken();
-    if (jwt) updateUserInfo(jwt);
-  }, []);
+	useEffect(() => {
+		const jwt = getJwtToken();
+		if (jwt) updateUserInfo(jwt);
+	}, []);
 
-  // Marquee effect duplication for infinite loop
-  useEffect(() => {
-    const marquee = document.querySelector('.marquee');
-    if (marquee) {
-      // Get the current content and store it as a data attribute
-      const content = marquee.innerHTML;
-      // Duplicate the content inside the marquee for continuous scrolling
-      // Adding it twice will make the -50% translateX work for a seamless loop
-      marquee.innerHTML = content + content;
-    }
-  }, []);
+	// Marquee effect duplication for infinite loop
+	useEffect(() => {
+		const marquee = document.querySelector('.marquee');
+		if (marquee) {
+			// Get the current content and store it as a data attribute
+			const content = marquee.innerHTML;
+			marquee.innerHTML = content + content;
+		}
+	}, []);
 
-  /** HANDLERS **/
-  const langClick = (e: any) => {
-    setAnchorEl2(e.currentTarget);
-  };
+	/** HANDLERS **/
+	const langClick = (e: any) => {
+		setAnchorEl2(e.currentTarget);
+	};
 
-  const langClose = () => {
-    setAnchorEl2(null);
-  };
+	const langClose = () => {
+		setAnchorEl2(null);
+	};
 
-  const langChoice = useCallback(
-    async (e: any) => {
-      setLang(e.target.id);
-      localStorage.setItem('locale', e.target.id);
-      setAnchorEl2(null);
-      await router.push(router.asPath, router.asPath, { locale: e.target.id });
-    },
-    [router],
-  );
-
-  const changeNavbarColor = () => {
-    if (window.scrollY >= 50) {
-      setColorChange(true);
-    } else {
-      setColorChange(false);
-    }
-  };
-
-
-  const StyledMenu = styled((props: MenuProps) => (
-	<Menu
-		elevation={0}
-		anchorOrigin={{
-			vertical: 'bottom',
-			horizontal: 'right',
-		}}
-		transformOrigin={{
-			vertical: 'top',
-			horizontal: 'right',
-		}}
-		{...props}
-	/>
-))(({ theme }) => ({
-	'& .MuiPaper-root': {
-		top: '109px',
-		borderRadius: 6,
-		marginTop: theme.spacing(1),
-		minWidth: 160,
-		color: theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
-		boxShadow:
-			'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
-		'& .MuiMenu-list': {
-			padding: '4px 0',
+	const langChoice = useCallback(
+		async (e: any) => {
+			setLang(e.target.id);
+			localStorage.setItem('locale', e.target.id);
+			setAnchorEl2(null);
+			await router.push(router.asPath, router.asPath, { locale: e.target.id });
 		},
-		'& .MuiMenuItem-root': {
-			'& .MuiSvgIcon-root': {
-				fontSize: 18,
-				color: theme.palette.text.secondary,
-				marginRight: theme.spacing(1.5),
+		[router],
+	);
+
+	const changeNavbarColor = () => {
+		if (window.scrollY >= 50) {
+			setColorChange(true);
+		} else {
+			setColorChange(false);
+		}
+	};
+
+	const StyledMenu = styled((props: MenuProps) => (
+		<Menu
+			elevation={0}
+			anchorOrigin={{
+				vertical: 'bottom',
+				horizontal: 'right',
+			}}
+			transformOrigin={{
+				vertical: 'top',
+				horizontal: 'right',
+			}}
+			{...props}
+		/>
+	))(({ theme }) => ({
+		'& .MuiPaper-root': {
+			top: '109px',
+			borderRadius: 6,
+			marginTop: theme.spacing(1),
+			minWidth: 160,
+			color: theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
+			boxShadow:
+				'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+			'& .MuiMenu-list': {
+				padding: '4px 0',
 			},
-			'&:active': {
-				backgroundColor: alpha(theme.palette.primary.main, theme.palette.action.selectedOpacity),
+			'& .MuiMenuItem-root': {
+				'& .MuiSvgIcon-root': {
+					fontSize: 18,
+					color: theme.palette.text.secondary,
+					marginRight: theme.spacing(1.5),
+				},
+				'&:active': {
+					backgroundColor: alpha(theme.palette.primary.main, theme.palette.action.selectedOpacity),
+				},
 			},
 		},
-	},
-}));
+	}));
 
-  if (typeof window !== 'undefined') {
-    window.addEventListener('scroll', changeNavbarColor);
-  }
+	if (typeof window !== 'undefined') {
+		window.addEventListener('scroll', changeNavbarColor);
+	}
 
-  if (device == 'mobile') {
-    return (
-      <Stack className={'top'}>
-        {/* Mobile Navigation Links */}
-        <Link href="/">
-          <div className={`nav-item ${router.pathname === '/' ? 'active' : ''}`}>
-            {t('Home')}
-            {router.pathname === '/' && (
-              <img src="/img/icons/page-locator.png" alt="Page Locator" className="page-locator" />
-            )}
-          </div>
-        </Link>
-        <Link href="/product">
-          <div className={`nav-item ${router.pathname === '/product' ? 'active' : ''}`}>
-            {t('Products')}
-            {router.pathname === '/product' && (
-              <img src="/img/icons/page-locator.png" alt="Page Locator" className="page-locator" />
-            )}
-          </div>
-        </Link>
-        <Link href="/store">
-          <div className={`nav-item ${router.pathname === '/store' ? 'active' : ''}`}>
-            {t('Stores')}
-            {router.pathname === '/store' && (
-              <img src="/img/icons/page-locator.png" alt="Page Locator" className="page-locator" />
-            )}
-          </div>
-        </Link>
-        <Link href="/community">
-          <div className={`nav-item ${router.pathname === '/community' ? 'active' : ''}`}>
-            {t('Community')}
-            {router.pathname === '/community' && (
-              <img src="/img/icons/page-locator.png" alt="Page Locator" className="page-locator" />
-            )}
-          </div>
-        </Link>
-        <Link href="/cs">
-          <div className={`nav-item ${router.pathname === '/cs' ? 'active' : ''}`}>
-            {t('CS')}
-            {router.pathname === '/cs' && (
-              <img src="/img/icons/page-locator.png" alt="Page Locator" className="page-locator" />
-            )}
-          </div>
-        </Link>
-      </Stack>
-    );
-  } else {
-    return (
+	if (device == 'mobile') {
+		return (
+			<Stack className={'top'}>
+				{/* Mobile Navigation Links */}
+				<Link href="/">
+					<div className={`nav-item ${router.pathname === '/' ? 'active' : ''}`}>
+						{t('Home')}
+						{router.pathname === '/' && (
+							<img src="/img/icons/page-locator.png" alt="Page Locator" className="page-locator" />
+						)}
+					</div>
+				</Link>
+				<Link href="/product">
+					<div className={`nav-item ${router.pathname === '/product' ? 'active' : ''}`}>
+						{t('Products')}
+						{router.pathname === '/product' && (
+							<img src="/img/icons/page-locator.png" alt="Page Locator" className="page-locator" />
+						)}
+					</div>
+				</Link>
+				<Link href="/store">
+					<div className={`nav-item ${router.pathname === '/store' ? 'active' : ''}`}>
+						{t('Stores')}
+						{router.pathname === '/store' && (
+							<img src="/img/icons/page-locator.png" alt="Page Locator" className="page-locator" />
+						)}
+					</div>
+				</Link>
+				<Link href="/community">
+					<div className={`nav-item ${router.pathname === '/community' ? 'active' : ''}`}>
+						{t('Community')}
+						{router.pathname === '/community' && (
+							<img src="/img/icons/page-locator.png" alt="Page Locator" className="page-locator" />
+						)}
+					</div>
+				</Link>
+				<Link href="/cs">
+					<div className={`nav-item ${router.pathname === '/cs' ? 'active' : ''}`}>
+						{t('CS')}
+						{router.pathname === '/cs' && (
+							<img src="/img/icons/page-locator.png" alt="Page Locator" className="page-locator" />
+						)}
+					</div>
+				</Link>
+			</Stack>
+		);
+	} else {
+		return (
 			<div className="navbar">
 				{/* Top Navbar */}
-				{/* The 'transparent' class should be conditionally applied if you have a scroll effect changing background */}
 				<div className={`navbar-main ${colorChange ? 'colored' : 'transparent'}`}>
 					<div className="container">
 						<div className="top-navbar">
@@ -261,107 +257,189 @@ const Top = () => {
 							<div className="bottom-center">
 								<div className="logo-box">
 									<Link href="/">
-										<img src="/img/logo/logo-name.svg" alt="Veloura-logo" />
+										<img src="/img/logo/black_on_white2.png" alt="Veloura Logo" className="logo" />
 									</Link>
 								</div>
 							</div>
 							<div className="bottom-right">
-							<Box component={'div'} className={'user-box'}>
-							{user?._id ? (
-								<>
-									<div className={'login-user'} onClick={(event: any) => setLogoutAnchor(event.currentTarget)}>
-										<img
-											src={
-												user?.memberImage ? `${REACT_APP_API_URL}/${user?.memberImage}` : '/img/profile/defaultUser3.svg'
-											}
-											alt=""
-										/>
+								<Box component={'div'} className={'user-box'}>
+									{user?._id ? (
+										<>
+											<div className={'login-user'} onClick={(event: any) => setLogoutAnchor(event.currentTarget)}>
+												<img
+													src={
+														user?.memberImage
+															? `${REACT_APP_API_URL}/${user?.memberImage}`
+															: '/img/profile/defaultUser3.svg'
+													}
+													alt=""
+												/>
+											</div>
+
+											<Menu
+												id="basic-menu"
+												anchorEl={logoutAnchor}
+												open={logoutOpen}
+												onClose={() => {
+													setLogoutAnchor(null);
+												}}
+												sx={{ mt: '5px' }}
+											>
+												<MenuItem onClick={() => logOut()}>
+													<Logout fontSize="small" style={{ color: 'blue', marginRight: '10px' }} />
+													Logout
+												</MenuItem>
+											</Menu>
+										</>
+									) : (
+										<Link href={'/account/join'}>
+											<div className={'join-box'}>
+												<span>
+													{t('Login')} / {t('Register')}
+												</span>
+											</div>
+										</Link>
+									)}
+
+									<div className="lan-box">
+										{user?._id && <NotificationsOutlinedIcon className="notification-icon" />}
+										<Button
+											disableRipple
+											className="btn-lang"
+											onClick={langClick}
+											endIcon={<CaretDown size={14} color="#616161" weight="fill" />}
+										>
+											<Box component="div" className="flag">
+												{lang !== null ? (
+													<img src={`/img/flag/lang${lang}.png`} alt={`${lang}Flag`} />
+												) : (
+													<img src={`/img/flag/langen.png`} alt="usaFlag" />
+												)}
+											</Box>
+										</Button>
+
+										<StyledMenu anchorEl={anchorEl2} open={drop} onClose={langClose}>
+											<MenuItem disableRipple onClick={langChoice} id="en">
+												<Box
+													component="div"
+													sx={{
+														width: '24px',
+														height: '18px',
+														borderRadius: '3px',
+														overflow: 'hidden',
+														display: 'flex',
+														alignItems: 'center',
+														justifyContent: 'center',
+														marginRight: '8px',
+													}}
+												>
+													<img
+														src="/img/flag/langen.png"
+														alt="usaFlag"
+														style={{
+															width: '100%',
+															height: '100%',
+															objectFit: 'cover',
+															display: 'block',
+														}}
+													/>
+												</Box>
+												{t('English')}
+											</MenuItem>
+
+											<MenuItem disableRipple onClick={langChoice} id="uz">
+												<Box
+													component="div"
+													sx={{
+														width: '24px',
+														height: '18px',
+														borderRadius: '3px',
+														overflow: 'hidden',
+														display: 'flex',
+														alignItems: 'center',
+														justifyContent: 'center',
+														marginRight: '8px',
+													}}
+												>
+													<img
+														src="/img/flag/uzb-flag.png"
+														alt="uzbekFlag"
+														style={{
+															width: '100%',
+															height: '100%',
+															objectFit: 'cover',
+															display: 'block',
+														}}
+													/>
+												</Box>
+												{t('Uzbek')}
+											</MenuItem>
+
+											<MenuItem disableRipple onClick={langChoice} id="kr">
+												<Box
+													component="div"
+													sx={{
+														width: '24px',
+														height: '18px',
+														borderRadius: '3px',
+														overflow: 'hidden',
+														display: 'flex',
+														alignItems: 'center',
+														justifyContent: 'center',
+														marginRight: '8px',
+													}}
+												>
+													<img
+														src="/img/flag/langkr.png"
+														alt="koreanFlag"
+														style={{
+															width: '100%',
+															height: '100%',
+															objectFit: 'cover',
+															display: 'block',
+														}}
+													/>
+												</Box>
+												{t('Korean')}
+											</MenuItem>
+
+											<MenuItem disableRipple onClick={langChoice} id="ru">
+												<Box
+													component="div"
+													sx={{
+														width: '24px',
+														height: '18px',
+														borderRadius: '3px',
+														overflow: 'hidden',
+														display: 'flex',
+														alignItems: 'center',
+														justifyContent: 'center',
+														marginRight: '8px',
+													}}
+												>
+													<img
+														src="/img/flag/langru.png"
+														alt="russianFlag"
+														style={{
+															width: '100%',
+															height: '100%',
+															objectFit: 'cover',
+															display: 'block',
+														}}
+													/>
+												</Box>
+												{t('Russian')}
+											</MenuItem>
+										</StyledMenu>
 									</div>
-
-									<Menu
-										id="basic-menu"
-										anchorEl={logoutAnchor}
-										open={logoutOpen}
-										onClose={() => {
-											setLogoutAnchor(null);
-										}}
-										sx={{ mt: '5px' }}
-									>
-										<MenuItem onClick={() => logOut()}>
-											<Logout fontSize="small" style={{ color: 'blue', marginRight: '10px' }} />
-											Logout
-										</MenuItem>
-									</Menu>
-								</>
-							) : (
-								<Link href={'/account/join'}>
-									<div className={'join-box'}>
-										<AccountCircleOutlinedIcon />
-										<span>
-											{t('Login')} / {t('Register')}
-										</span>
-									</div>
-								</Link>
-							)}
-
-							<div className={'lan-box'}>
-								{user?._id && <NotificationsOutlinedIcon className={'notification-icon'} />}
-								<Button
-									disableRipple
-									className="btn-lang"
-									onClick={langClick}
-									endIcon={<CaretDown size={14} color="#616161" weight="fill" />}
-								>
-									<Box component={'div'} className={'flag'}>
-										{lang !== null ? (
-											<img src={`/img/flag/lang${lang}.png`} alt={'usaFlag'} />
-										) : (
-											<img src={`/img/flag/langen.png`} alt={'usaFlag'} />
-										)}
-									</Box>
-								</Button>
-
-								<StyledMenu anchorEl={anchorEl2} open={drop} onClose={langClose} sx={{ position: 'absolute' }}>
-									<MenuItem disableRipple onClick={langChoice} id="en">
-										<img
-											className="img-flag"
-											src={'/img/flag/langen.png'}
-											onClick={langChoice}
-											id="en"
-											alt={'usaFlag'}
-										/>
-										{t('English')}
-									</MenuItem>
-									<MenuItem disableRipple onClick={langChoice} id="kr">
-										<img
-											className="img-flag"
-											src={'/img/flag/langkr.png'}
-											onClick={langChoice}
-											id="uz"
-											alt={'koreanFlag'}
-										/>
-										{t('Korean')}
-									</MenuItem>
-									<MenuItem disableRipple onClick={langChoice} id="ru">
-										<img
-											className="img-flag"
-											src={'/img/flag/langru.png'}
-											onClick={langChoice}
-											id="ru"
-											alt={'russiaFlag'}
-										/>
-										{t('Russian')}
-									</MenuItem>
-								</StyledMenu>
-							</div>
-						</Box>
+								</Box>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 		);
-  }
+	}
 };
 
 export default withRouter(Top);
--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	i18n: {
 		defaultLocale: 'en',
-		locales: ['en', 'kr', 'ru'],
+		locales: ['en', 'uz', 'kr', 'ru'],
 		localeDetection: false,
 	},
 	trailingSlash: true,
